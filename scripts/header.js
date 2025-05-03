08b91c26c55bdfae40131d17620ccd6425b31de9
ObjectOfClassMateusz <matitr2709@tutanota.com>
2025-05-03 15:06:03 +0200

    Added side-navigation

---
+++ scripts/header.js
@@ -0,0 +1,21 @@
+function backNavShowsUp()
+{
+    var backNavVariable = document.getElementById("backNavId");
+    var backNavButtonVariable = document.getElementById("backNavButtId");
+    
+    if (backNavVariable.style.visibility == "") 
+    {
+        backNavVariable.style.visibility = "visible";
+        backNavVariable.style.width = "40vw";
+        backNavButtonVariable.style.transition = "0.5s";
+        backNavButtonVariable.style.visibility = "visible";
+    }
+    else 
+    {
+        //console.log("Go home")
+        backNavVariable.style.visibility = "";
+        backNavVariable.style.width = "0px";
+        backNavButtonVariable.style.visibility = "";
+        backNavButtonVariable.style.transition = "0s";
+    }    
+}
