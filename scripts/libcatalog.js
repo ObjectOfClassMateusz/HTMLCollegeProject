04199afd1a729e57381ec4c67b6914f015cbcffd
Dominik J <dominik139j@gmail.com>
2025-05-25 13:54:03 +0200

    library catalog buttons

--- scripts/libcatalog.js
+++ scripts/libcatalog.js
@@ -1,7 +1,37 @@
+let catalogForm = document.getElementById('libcatalog-form')
+
 let searchbar = document.getElementById('searchphrase')
+
+let alphabeticalSortButton = document.getElementById('alphabetical')
+let countAscendingSortButton = document.getElementById('count-ascending')
+let countDescendingSortButton = document.getElementById('count-descending')
+
+let forestCategoryButton = document.getElementById('forests')
+let informaticsCategoryButton = document.getElementById('informatics')
+let mathsCategoryButton = document.getElementById('maths')
+let architectureCategoryButton = document.getElementById('architecture')
+let economyCategoryButton = document.getElementById('economy')
+let philosophyCategoryButton = document.getElementById('philosophy')
+
 let clearButton = document.getElementById('clearbutton')
+
+
+
+catalogForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
 
 clearButton.addEventListener("click", (event) => {
     event.preventDefault()
+
     searchbar.value = ''
+
+    alphabeticalSortButton.checked = true
+
+    forestCategoryButton.checked = true
+    informaticsCategoryButton.checked = true
+    mathsCategoryButton.checked = true
+    architectureCategoryButton.checked = true
+    economyCategoryButton.checked = true
+    philosophyCategoryButton.checked = true
 })
