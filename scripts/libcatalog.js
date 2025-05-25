fae0a2bbccbc97d09a5b29ad0974a7aaeec5559e
Dominik J <dominik139j@gmail.com>
2025-05-25 21:42:04 +0200

    Katalog książek

--- scripts/libcatalog.js
+++ scripts/libcatalog.js
@@ -23,12 +23,18 @@ let clearButton = document.getElementById('clearbutton')
 let categoryTextOutput = document.getElementById('categories-output')
 let sortingTextOutput = document.getElementById('sorting-output')
 
+let booksOutput = document.getElementById('library-books-list')
 
-const booksUrl = '/library-books'
+
+const booksUrl = 'http://localhost:3000/library-books'
+
+let books = []
 
 
 catalogForm.addEventListener("submit", (event) => {
     event.preventDefault()
+
+    updateBooksDisplay()
 })
 
 clearButton.addEventListener("click", (event) => {
@@ -47,17 +53,23 @@ clearButton.addEventListener("click", (event) => {
 
     updateCategoryState()
     updateSortingState()
+
+    updateBooksDisplay()
 })
 
 for (let radio of sortingRadios) {
     radio.addEventListener("change", (event) => {
         updateSortingState()
+
+        updateBooksDisplay()
     })
 }
 
 for (let box of categoryCheckboxes) {
     box.addEventListener("click", (event) => {
         updateCategoryState()
+
+        updateBooksDisplay()
     })
 }
 
@@ -101,8 +113,126 @@ function fetchBooks() {
     .then(response => response.json())
     .then(data => {
         for (let book of data)
-            console.log(book)
+            books.push(book)
+
+        updateBooksDisplay()
     })
+    .catch(error => console.log(error))
+}
+
+
+function filterBooks() {
+    let filteredBooks = books.filter((value) => {
+        
+        if (value.category === 'forests' && forestCategoryButton.checked === true)
+            return true
+        if (value.category === 'informatics' && informaticsCategoryButton.checked === true)
+            return true
+        if (value.category === 'maths' && mathsCategoryButton.checked === true)
+            return true
+        if (value.category === 'architecture' && architectureCategoryButton.checked === true)
+            return true
+        if (value.category === 'economy' && economyCategoryButton.checked === true)
+            return true
+        if (value.category === 'philosophy' && philosophyCategoryButton.checked === true)
+            return true
+        return false
+
+    }).filter((value) => {
+
+        if (searchbar.value === "")
+            return true
+
+        return value.title.toLowerCase().includes(searchbar.value.toLowerCase())
+    })
+
+    let sortedBooks = filteredBooks.sort((a, b) => {
+        if (alphabeticalTitleSortButton.checked === true)
+        {
+            return a.title > b.title
+        }
+        else if (alphabeticalAuthorSortButton.checked === true)
+        {
+            return a.authors > b.authors
+        }
+        else if (countAscendingSortButton.checked === true)
+        {
+            return a.count > b.count
+        }
+        else if (countDescendingSortButton.checked === true)
+        {
+            return a.count < b.count
+        }
+    })
+
+    return sortedBooks
+}
+
+function categoryToDisplayName(category) {
+    switch (category)
+    {
+    case 'forests':
+        return 'Leśnictwo'
+    case 'informatics':
+        return 'Informatyka'
+    case 'maths':
+        return 'Matematyka'
+    case 'architecture':
+        return 'Architektura'
+    case 'economy':
+        return 'Ekonomia'
+    case 'philosophy':
+        return 'Filozofia'
+    default:
+        return 'Inna'
+    }
+}
+
+
+function updateBooksDisplay() {
+    while (booksOutput.firstChild)
+        booksOutput.removeChild(booksOutput.firstChild)
+
+    let filteredBooks = filterBooks()
+
+    if (filteredBooks.length === 0) {
+        let noBooksMsg = document.createElement("H1")
+        noBooksMsg.textContent = 'Nie znaleziono żadnych książek dla danych filtrów!'
+        noBooksMsg.style.textAlign = 'center'
+        booksOutput.appendChild(noBooksMsg)
+    }
+    
+    for (let book of filteredBooks) {
+        let item = document.createElement("DIV")
+
+        let title = document.createElement("H1")
+        title.textContent = book.title
+        title.classList.add('book-title')
+        item.appendChild(title)
+
+        let authors = document.createElement("SPAN")
+        if (book.authors.includes(','))
+            authors.textContent = 'Autorzy: ' + book.authors
+        else
+            authors.textContent = 'Autor: ' + book.authors
+        item.appendChild(authors)
+
+        item.appendChild(document.createElement('BR'))
+
+        let category = document.createElement("SPAN")
+        category.textContent = 'Kategoria: ' + categoryToDisplayName(book.category)
+        item.appendChild(category)
+
+        item.appendChild(document.createElement('BR'))
+
+        let count = document.createElement("SPAN")
+        count.textContent = 'Liczba egzemplarzy: ' + book.count
+        item.appendChild(count)
+
+        item.appendChild(document.createElement("HR"))
+
+        booksOutput.append(item)
+    }
 }
 
 
